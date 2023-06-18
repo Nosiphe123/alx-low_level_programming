3be3539a17254c15a3deb38345ce580b2faa2008
Patience <patiencetyhalisi@gmail.com>
2023-06-18 08:10:14 -0700

    Programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    printf("Programming is like building a multilingual puzzle\n");
-    return 0;
+/**
+* main - Prints using printf() command
+* Return: 0 prints phrase
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
