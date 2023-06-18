8e933cae96553ff25cf315cbc0d19faf8587679a
Patience <patiencetyhalisi@gmail.com>
2023-06-18 08:05:56 -0700

    Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    printf("Programming is like building a multilingual puzzle\n");
-    return 0;
+/**
+* main - Prints using puts() command
+* Return: 0 prints something
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
