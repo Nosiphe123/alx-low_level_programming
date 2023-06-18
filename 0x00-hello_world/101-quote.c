cb28e758bb2a237a245ff0add2861bf4f48d3b12
Patience <patiencetyhalisi@gmail.com>
2023-06-18 08:20:34 -0700

    Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
 
 /**
-* main-Entry point
-*
-* Return:Always 1(Success)
-*/
+ * main - Main program
+ *
+ * Return: 1
+ */
+
+
 int main(void)
 {
-	write(2,
-       
-	"and that of art is useful\"-Dora Kopar
-	2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	   return(1);
+	return (1);
 }
